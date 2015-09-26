30980ec950cb0494944014dcbd21cfcd5ac144fc
Daniel Teske <daniel.teske@meteogroup.com>
2015-09-26 22:06:34 +0200

    new: gribfiles_are_detected_properly

--- src/test/java/com/meteogroup/grib2geotiff/grib/GribFileAccessTest.java
+++ src/test/java/com/meteogroup/grib2geotiff/grib/GribFileAccessTest.java
@@ -1,7 +1,13 @@
 package com.meteogroup.grib2geotiff.grib;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import ucar.unidata.io.RandomAccessFile;
+
+import java.io.File;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,10 +15,18 @@ import static org.junit.Assert.*;
  */
 public class GribFileAccessTest {
 
+    private static final String IN = "src/main/resources/GoldenTestData/";
+
     @Test
-    @Ignore
-    public void gribfiles_are_detected_properly(){
-        fail("not jet implemented");
+    public void gribfiles_are_detected_properly() throws Exception{
+        //given
+        GribFileAccess grib = new GribFileAccess();
+        System.out.println("path: " + new File(IN).getAbsolutePath());
+        grib.setInputDirectory(new File(IN));
+        //when
+        List<RandomAccessFile> validGribFiles = grib.getRafFiles();
+        //then
+        assertEquals(3,validGribFiles.size());
     }
 
 }
